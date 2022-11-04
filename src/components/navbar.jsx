de4871ff8cb658487bbb3f0a12e064a66dbb9a44
unknown <hafedhpes@gmail.com>
2022-11-04 22:19:38 +0100

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,57 +1,27 @@
-import React, { useState } from "react";
-//import { MenuIcon } from '@heroicons/react/outline'
-import { Bars3Icon } from '@heroicons/react/24/outline'
-import { XMarkIcon } from '@heroicons/react/24/outline'
- const Navbar= ()=>{
-    const [nav,setNav]=useState(false)
-    const handleClick=()=>{
-        setNav(!nav)
-    }
+import React from "react";
+import { UserIcon } from '@heroicons/react/24/outline'
+
+const Navbar= ()=>{
 return(
-    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
-        <div className="px-2 flex justify-between items-center h-full w-full">
-            <div className="flex items-center ">
-                <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Negociateur.tn</h1>
-                <ul className="hidden md:flex">
-
-                    <li>Accueil</li>
-                    <li>A propos</li>
-                    <li>Prix</li>
-                    <li>support</li>
-                </ul>
-            </div>
-            <div className="hidden md:flex pr-4">
-                <button className="p-2 border-none bg-transparent text-black mr-4">S'inscrire</button>
-                <button className="px-4 py-1">Connexion</button>
-
-            </div>
-            <div className="md:hidden" onClick={handleClick}>
-                {!nav?
-            <Bars3Icon className="w-5"/>
-                :(<XMarkIcon className="w-5"/>)}
-
-
-            </div>
-        </div>
-    
-        {nav&&(
-        <ul className={!nav? "hidden" :"absolute w-full bg-zinc-200 px-8"}>
-        <li className="border-b-2 border-zinc-300">Accueil</li>
-        <li className="border-b-2 border-zinc-300">A propos</li>
-        <li className="border-b-2 border-zinc-300">Prix</li>
-        <li className="border-b-2 border-zinc-300">support</li>
-
-        <div className="flex flex-col my-4">
-        <button className="p-2 bg-transparent text-indigo-600 px-8 py-3 mb-4">S'inscrire</button>
-        <button className="px-8 py-3">Connexion</button>
-        </div>
-        </ul>
-        )}
-        
-
-        
+    <div className=" drop-shadow-md flex fixed justify-between items-center w-screen h-[65px] bg-white border-b border-gray-100">
+    <div className="hidden sm:flex sm:w-3/12    px-2">
+    <h1 className=" sm:text-3xl text-[#eb6b56] font-bold">Negociateur</h1>
+    </div>
+    <div className="flex justify-center w-3/12 sm:hidden     px-2">
+    <h1 className="  sm:text-3xl text-[#eb6b56] font-bold">Negociateur</h1>
+    </div>
+    <div className="flex justify-center  w-4/12 "> 
+    <input type="text" className="w-3/6 focus:w-5/6  text-black placeholder:text-gray-300 text-lg border border-[#eb6b56] outline-none rounded-lg px-2 py-2" placeholder="Recherche"/>
+    </div>
+    <div className="hidden sm:flex w-2/12  ">
+        <button className="px-4 py-2 rounded-lg hover:text-[#eb6b56]"> + Déposer une annonce </button>
+    </div>
+    <div className="hidden sm:flex justify-center w-3/12">
+        <button className="bg-transparent text-black px-2 py-2 hover:px-3 hover:py-3"><UserIcon className="w-5 inline mr-2"/>
+        Se connecter </button></div>
     
     </div>
 )
- }
- export default Navbar
+
+}
+export default Navbar
