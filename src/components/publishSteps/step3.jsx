243a4474475d60d4d6e8f021aa0a1aa858ee3591
unknown <hafedhpes@gmail.com>
2022-11-12 20:45:38 +0100

    step3

--- src/components/publishSteps/step3.jsx
+++ src/components/publishSteps/step3.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import {React,useState} from "react";
 
 const Step3= ()=>{
 
-
+const [numStatus,setNumStatus]=useState(true)
+const handleChange= e=>{
+setNumStatus(e.target.checked)
+console.log(numStatus)
+}
     return(
         <>
         
@@ -26,18 +30,22 @@ const Step3= ()=>{
     </div>
 </div>
 
-<div className="mt-4 mb-4 grid grid-cols-2 gap-2">
+
+<div className="mt-4  grid grid-cols-2 gap-2">
     <p className="text-sm text-gray-500">Utiliser mon numéro principal ?</p>
     <div className="flex justify-end">
     <label flex for="checked-toggle" className="inline-flex relative items-center cursor-pointer">
-  <input type="checkbox" defaultChecked value="" id="checked-toggle" class="sr-only peer" />
+  <input onChange={handleChange} type="checkbox"  checked={numStatus} id="checked-toggle" class="sr-only peer" name="tel" />
   <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#eb6b56]"></div>
   
 </label>
 </div>
 </div>
-
-        
+{ !numStatus &&<>
+<label className="mt-4 text-gray-500 font-bold">Tel</label>
+<input  type="number" className="mt-2 bg-gray-100 text-gray-500 px-4 py-3 outline-none rounded-sm"/>
+</>
+ }
         </>
     )
 
