5ddedac8e88ef5cf0552aed437d888f25d24790d
unknown <hafedhpes@gmail.com>
2022-12-17 12:59:04 +0100

    home reponsive

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,6 +6,7 @@ import testImg2 from "../../assets/posts/test2.jpg"
 import rightIcon from "../../assets/right.svg"
 import leftIcon from "../../assets/left.svg"
 import starIcon from "../../assets/star.svg"
+import locIcon from "../../assets/location.svg"
 const Home=()=>{
 const slider= useRef()
 const scrollRight=()=>{
@@ -18,7 +19,7 @@ return(<>
 <Navbar/>
 
 <div className="flex w-full mt-4 justify-evenly">
-<div className="w-[10%]">
+<div className="hidden sm:flex w-[10%]">
 <Cats/>
 </div>
 <div className="flex flex-col w-[80%] ">
@@ -29,9 +30,9 @@ return(<>
     <span className="text-2xl font-bold px-1">A la une</span>
     </div>
 
-    <div className="flex relative pr-10 mt-3">
-        <div ref={slider}  className="w-full h-full  overflow-x-scroll scroll  whitespace-nowrap scroll-smooth scrollbar-hide">
-     <div className="w-[25%] h-[400px] inline-block p-2 whitespace-normal">
+    <div className="flex  pr-10 mt-3">
+        <div ref={slider}  className=" relative  h-full  overflow-x-scroll scroll  whitespace-nowrap scroll-smooth scrollbar-hide">
+     <div className=" h-[400px] w-full sm:w-2/4 md:w-1/4 inline-block p-2 whitespace-normal">
         <div className="h-[50%]">
             <img src={testImg2} className="w-full h-[100%] object-cover"/>
         </div>
@@ -48,16 +49,17 @@ return(<>
         <span className="px-2 mt-1 text-sm text-gray-500">
         Jeux vidéos
         </span>
-        <span className="px-2 mt-1 mb-2 text-sm text-gray-500">
+        <span className="px-2 mt-1 mb-2 text-sm text-gray-500 flex items-center">
+        <img src={locIcon} className="w-3 h-3"/>
         Ariana , il y a 3 jours 
         </span>
         </div> 
 
      </div>
 
-     <div className="w-[25%] h-[400px] inline-block p-2">
+     <div className=" h-[400px] w-full sm:w-2/4 md:w-1/4 inline-block p-2 whitespace-normal">
         <div className="h-[50%]">
-            <img src={testImg} className="w-full h-[100%] object-cover"/>
+            <img src={testImg2} className="w-full h-[100%] object-cover"/>
         </div>
         <div className="flex flex-col bg-gray-100 h-[20%]" >
         <span className="mt-1 text-lg text-[#eb6b56] font-bold px-2">
@@ -72,17 +74,17 @@ return(<>
         <span className="px-2 mt-1 text-sm text-gray-500">
         Jeux vidéos
         </span>
-        <span className="px-2 mt-1 mb-2 text-sm text-gray-500">
+        <span className="px-2 mt-1 mb-2 text-sm text-gray-500 flex items-center">
+        <img src={locIcon} className="w-3 h-3"/>
         Ariana , il y a 3 jours 
         </span>
         </div> 
 
      </div>
 
-
-     <div className="w-[25%] h-[400px] inline-block p-2">
+     <div className=" h-[400px] w-full sm:w-2/4 md:w-1/4 inline-block  p-2 whitespace-normal">
         <div className="h-[50%]">
-            <img src={testImg} className="w-full h-[100%] object-cover"/>
+            <img src={testImg2} className="w-full h-[100%] object-cover"/>
         </div>
         <div className="flex flex-col bg-gray-100 h-[20%]" >
         <span className="mt-1 text-lg text-[#eb6b56] font-bold px-2">
@@ -97,17 +99,17 @@ return(<>
         <span className="px-2 mt-1 text-sm text-gray-500">
         Jeux vidéos
         </span>
-        <span className="px-2 mt-1 mb-2 text-sm text-gray-500">
+        <span className="px-2 mt-1 mb-2 text-sm text-gray-500 flex items-center">
+        <img src={locIcon} className="w-3 h-3"/>
         Ariana , il y a 3 jours 
         </span>
         </div> 
 
      </div>
 
-
-     <div className="w-[25%] h-[400px] inline-block p-2">
+     <div className=" h-[400px] w-full sm:w-2/4 md:w-1/4 inline-block p-2 whitespace-normal">
         <div className="h-[50%]">
-            <img src={testImg} className="w-full h-[100%] object-cover"/>
+            <img src={testImg2} className="w-full h-[100%] object-cover"/>
         </div>
         <div className="flex flex-col bg-gray-100 h-[20%]" >
         <span className="mt-1 text-lg text-[#eb6b56] font-bold px-2">
@@ -122,59 +124,66 @@ return(<>
         <span className="px-2 mt-1 text-sm text-gray-500">
         Jeux vidéos
         </span>
-        <span className="px-2 mt-1 mb-2 text-sm text-gray-500">
+        <span className="px-2 mt-1 mb-2 text-sm text-gray-500 flex items-center">
+        <img src={locIcon} className="w-3 h-3"/>
         Ariana , il y a 3 jours 
         </span>
         </div> 
 
      </div>
+
+     <div className=" h-[400px] w-full sm:w-2/4 md:w-1/4 inline-block p-2 whitespace-normal">
+        <div className="h-[50%]">
+            <img src={testImg2} className="w-full h-[100%] object-cover"/>
+        </div>
+        <div className="flex flex-col bg-gray-100 h-[20%]" >
+        <span className="mt-1 text-lg text-[#eb6b56] font-bold px-2">
+            99 DT
+        </span>
+        <span className="px-2 text-md text-black">
+        JEUX SWITCH dispo chez GAME ZONE ariana bla laa blaa
+        </span>
+        </div>
+        <div className="h-[15%] bg-gray-100 flex flex-col">
+        <p className=" px-2 m-auto w-[92%] m-l-2 border border-b-2"></p>
+        <span className="px-2 mt-1 text-sm text-gray-500">
+        Jeux vidéos
+        </span>
+        <span className="px-2 mt-1 mb-2 text-sm text-gray-500 flex items-center">
+        <img src={locIcon} className="w-3 h-3"/>
+        Ariana , il y a 3 jours 
+        </span>
+        </div> 
+
+     </div>
+
+     
+
     
-
-     <div className="w-[25%] h-[400px] inline-block p-2">
-        <div className="h-[50%]">
-            <img src={testImg2} className="w-full h-[100%] object-cover"/>
-        </div>
-        <div className="flex flex-col bg-gray-100 h-[20%]" >
-        <span className="mt-1 text-lg text-[#eb6b56] font-bold px-2">
-            99 DT
-        </span>
-        <span className="px-2 text-md text-black">
-        JEUX SWITCH dispo chez 
-        </span>
-        </div>
-        <div className="h-[15%] bg-gray-100 flex flex-col">
-        <p className=" px-2 m-auto w-[92%] m-l-2 border border-b-2"></p>
-        <span className="px-2 mt-1 text-sm text-gray-500">
-        Jeux vidéos
-        </span>
-        <span className="px-2 mt-1 mb-2 text-sm text-gray-500">
-        Ariana , il y a 3 jours 
-        </span>
-        </div>
-        
-
    
      
       
         
 
-     </div>
+    
     
 
 
    
    
-
+     
      
 
     </div>
-    
-        <div onClick={scrollRight}className="bg-[#eb6b56] rounded-full absolute right-7 top-[15%] px-2 py-2 cursor-pointer ">
-     <img className="w-10" src={rightIcon}/>
+    <div className="absolute  top-[38%] right-14  sm:top-[29%] sm:right-14">
+    <div onClick={scrollRight}className="bg-[#eb6b56] rounded-full  px-2 py-2 cursor-pointer ">
+     <img className="" src={rightIcon}/>
         </div>
-        <div onClick={scrollLeft} className="bg-white rounded-full shadow-lg absolute right-9 top-[30%] px-2 py-2 cursor-pointer ">
+        <div onClick={scrollLeft} className="bg-white rounded-full shadow-lg w-[50%] mt-2 ml-4  px-2 py-2 cursor-pointer ">
      <img className="w-5" src={leftIcon}/>
         </div>
+        </div>
+        
     
 
 
